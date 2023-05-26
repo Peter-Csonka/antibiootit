607d95877a66a83fdd2a6a1190d3cc00d515fe56
Roosa Rintamäki <roosa.rintamaki@tuni.fi>
2023-05-26 10:45:50 +0300

    improved About the page scalability

--- frontend/antibiootit/src/components/Feedback.js
+++ frontend/antibiootit/src/components/Feedback.js
@@ -12,7 +12,7 @@ export default function Feedback() {
                 title="feedback"
                 className="feedback-form"
                 src="https://docs.google.com/forms/d/e/1FAIpQLSdnDoQe0D4rqPg2T8SC1Ezm8-_Mp7PxNxVrd9be42y8RzAduA/viewform?embedded=true" 
-                width="640" 
+                width="700" 
                 height="1488"
                 onLoad={() => setShowLoadingIndicator(false)}>
             </iframe>
