d73d0ec112cb522b93b0051fd612344c91c42086
Roosa Rintamäki <roosa.rintamaki@tuni.fi>
2023-06-06 13:59:13 +0300

    Started doing cookies pop-up

---
+++ frontend/antibiootit/src/components/Cookies.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CookiePopup = () => {
+  return (
+    <div className="cookie-popup">
+      <p>Tämä sivusto käyttää evästeitä parantaakseen käyttökokemustasi.</p>
+      <button>Hyväksy</button>
+    </div>
+  );
+}
+
+export default CookiePopup;
