59f202f28efe1bb3cabf2ecf519d965dfb25a711
Roosa Rintamäki <roosa.rintamaki@tuni.fi>
2023-06-08 11:48:46 +0300

    Fixed some scalability problems

--- frontend/antibiootit/src/App.js
+++ frontend/antibiootit/src/App.js
@@ -2,23 +2,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
 import { Routes, Route } from "react-router-dom";
-import CookiePopup from './components/Cookies';
-import React, { useState } from 'react';
-
 
 function App() {
-
-  const [showCookiePopup, setShowCookiePopup] = useState(true);
-
-  const handleAcceptCookies = () => {
-    setShowCookiePopup(false);
-    // Tässä vaiheessa voit tallentaa käyttäjän hyväksynnän esimerkiksi
-    // paikalliseen tilaan tai evästeisiin.
-  };
-
   return (
     <>
-      {showCookiePopup && <CookiePopup onAccept={handleAcceptCookies} />}
       <Header />
       <Routes>
 				<Route path="/" element={<Main path = "/" />} />
