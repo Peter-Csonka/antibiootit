faeef6172a21e2cbb61410bf29d146bb157eb7bc
Peter-Csonka <136794932+Peter-Csonka@users.noreply.github.com>
2023-07-07 10:24:46 +0300

    Update Main.js

--- frontend/antibiootit/src/components/Main.js
+++ frontend/antibiootit/src/components/Main.js
@@ -23,7 +23,8 @@ export default function Main(props) {
                 <aside>
                     <p className="aside-text">Antibiootit.fi on terveydenhuollon ammattilaisten käyttöön suunniteltu antibioottilaskuri, joka laskee suositellun antibioottiannostuksen diagnoosin ja lapsen painon perusteella. Suositukset perustuvat Käypä hoito -suosituksiin. </p>
                     <p className="aside-update">Versio 1.0 julkaistu 2.6.2023</p>
-                    <p className="aside-version-update">Viimeisin päivitys 14.6.2023</p>
+                    <p className="aside-update">Versio 1.1 julkaistu 6.7.2023</p>
+                    <p className="aside-version-update">Viimeisin päivitys 7.7.2023</p>
                 </aside>
             </main>}
 
