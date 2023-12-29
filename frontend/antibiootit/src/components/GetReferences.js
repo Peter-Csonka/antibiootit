932d02810dc3506b00c33268d9433e2a97fb7970
Juho Rantala <juho.rantala@live.com>
2023-12-29 18:46:19 +0200

    Removed unused texts

--- frontend/antibiootit/src/components/GetReferences.js
+++
@@ -1,8 +0,0 @@
-import references from "../data/references.json";
-
-export default async function getReferences() {
-    const referencesList = references.map(item => {
-        return (item)
-    })
-    return referencesList;
-}
